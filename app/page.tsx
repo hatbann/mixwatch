441343af8f26458f082285c32ad5f0754c82c64f
hatbann <0urloveis_great@naver.com>
2023-08-19 21:05:27 +0900

    (feat) Change select position checkbox to radio btn

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 /** @format */
-"use client";
+'use client';
 
-import styles from "./page.module.css";
-import { playerInputType, usePlayerForm } from "./form/form.player";
-import { useState } from "react";
+import styles from './page.module.scss';
+import { playerInputType, usePlayerForm } from './form/form.player';
+import { useState, useRef, useEffect, MouseEventHandler } from 'react';
 
 export default function Home() {
   const {
@@ -15,48 +15,72 @@ export default function Home() {
     r,
   } = usePlayerForm();
   const [players, setPlayers] = useState<playerInputType[]>([]);
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const handleClick = () => {
+    console.log(formRef.current?.teamType.value);
+  };
 
   return (
     <main className={styles.main}>
       <section>
         <h2>정보입력</h2>
-        <input type="checkbox" id="balance" name="teamType" />
-        <label htmlFor="balance">밸런스</label>
-        <input type="checkbox" id="similar" name="teamType" />
-        <label htmlFor="similar">비슷한 유저끼리</label>
-        <form action="">
-          <div>
-            <input type="text" {...r.name} placeholder="이름을 입력해주세요" />
-            {errors.name ? <p>{errors.name.message}</p> : <></>}
+        <form action="" ref={formRef}>
+          <div className={styles.selectTeamType}>
+            <input type="radio" id="balance" name="teamType" value="balance" />
+            <label htmlFor="balance">밸런스</label>
+            <input type="radio" id="similar" name="teamType" value="similar" />
+            <label htmlFor="similar">비슷한 유저끼리</label>
           </div>
-          <div>
-            <input
-              type="checkbox"
-              {...r.position}
-              id="tanker"
-              name="position"
-            />
-            <label htmlFor="tanker">탱커</label>
+          <div className={styles.inputTexts}>
+            <div>
+              <input
+                type="text"
+                {...r.name}
+                placeholder="이름을 입력해주세요"
+              />
+              {errors.name ? <p>{errors.name.message}</p> : <></>}
+            </div>
+            <div>
+              <input
+                type="text"
+                {...r.playTime}
+                placeholder="플레이 시간을 입력해주세요"
+              />
+              {errors.playTime ? <p>{errors.playTime.message}</p> : <></>}
+            </div>
           </div>
-          <div>
-            <input
-              type="checkbox"
-              {...r.position}
-              id="dealer"
-              name="position"
-            />
-            <label htmlFor="dealer">딜러</label>
-          </div>
-          <div>
-            <input
-              type="checkbox"
-              {...r.position}
-              id="healer"
-              name="position"
-            />
-            <label htmlFor="healer">힐러</label>
+          <div className={styles.postionChecks}>
+            <div>
+              <input
+                type="checkbox"
+                {...r.position}
+                id="tanker"
+                name="position"
+              />
+              <label htmlFor="tanker">탱커</label>
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                {...r.position}
+                id="dealer"
+                name="position"
+              />
+              <label htmlFor="dealer">딜러</label>
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                {...r.position}
+                id="healer"
+                name="position"
+              />
+              <label htmlFor="healer">힐러</label>
+            </div>
           </div>
         </form>
+        <button onClick={handleClick}>클릭</button>
       </section>
       <section>
         {players.map((player, idx) => (
